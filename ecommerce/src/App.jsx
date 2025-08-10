ae7cdab74872abafb6a650c9af76a6cd642e254e
Rohit Kumar Pal <rkp7133@gmail.com>
2025-08-10 22:36:04 +0530

    10/08/2025 sum up

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrders from "./pages/PlaceOrders";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
     <>
-
-       
-
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products/:productId" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/placeorders" element={<PlaceOrders />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
